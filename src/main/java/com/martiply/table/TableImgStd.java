b159cc693bc996e878409e8f38dc654a803475db
Martin <martin@hanatoya.jp>
2018-12-05 20:36:47 +0900

    Changed URL to PATH

--- src/main/java/com/martiply/table/TableImgStd.java
+++ src/main/java/com/martiply/table/TableImgStd.java
@@ -5,11 +5,11 @@ public class TableImgStd {
 	public static final String TABLE_NAME = "img_standard";
 	
 	public static final String ID = TableStandard.ID;
-	public static final String URL = "url";
+	public static final String PATH = "path";
 	public static final String TS = "ts";
 	
 	public static final String ID_F = TABLE_NAME + "." + ID;
-	public static final String URL_F = TABLE_NAME + "." + URL;
+	public static final String PATH_F = TABLE_NAME + "." + PATH;
 	public static final String TS_F = TABLE_NAME + "." + TS;
 
 }
